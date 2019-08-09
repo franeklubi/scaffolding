6672ed8cd24cfae9be55dc513f264f1815deeae2
franek <franeklubi@gmail.com>
2019-08-09 22:13:28 +0200

    Add getSize; Refactor HeadPos -> Head

--- main.c
+++ main.c
@@ -6,20 +6,26 @@ typedef struct {
     uint64_t y;
     char op;
     uint64_t mod;
-} HeadPos;
+} Head;
 
 
-HeadPos genHead() {
-    HeadPos h = {0, 0, '\0', 0};
+Head genHead() {
+    Head h = {0, 0, '\0', 0};
     return h;
 }
 
-
-int main() {
-    HeadPos r_head_pos = genHead();
-    HeadPos w_head_pos = genHead();
-
-    FILE* file = fopen("hw.scaf", "r+");
-    fclose(file);
+uint32_t getSize(FILE* file) {
+    fseek(file, 0L, SEEK_END);
+    return ftell(file);
 }
 
+int main() {
+    Head r_head_pos = genHead();
+    Head w_head_pos = genHead();
+
+    FILE* file = fopen("hw.scaf", "r");
+
+        int32_t file_len = getSize(file);
+
+    fclose(file);
+}
