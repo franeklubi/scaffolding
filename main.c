f2c72006b3495e5512f22aee67059885d62ed9d4
franek <franeklubi@gmail.com>
2019-08-10 00:07:22 +0200

    Add interpret function

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 typedef struct {
     uint64_t x;
@@ -38,21 +39,32 @@ char* loadFile(FILE* file, uint32_t file_size) {
     return start_ptr;
 }
 
+bool interpret(
+    char* buffer_ptr, uint32_t buffer_len, Head r_head, Head w_head
+) {
+
+
+
+
+
+    return true;
+}
 
 
 int main() {
-    Head r_head_pos = genHead();
-    Head w_head_pos = genHead();
+    Head r_head = genHead();
+    Head w_head = genHead();
 
     FILE* file = fopen("hw.scaf", "r");
-
         int32_t file_size = getSize(file);
         char* start_ptr = loadFile(file, file_size);
-        for ( uint32_t x = 0; x < file_size; x++ ) {
-            printf("%c\n", start_ptr[x]);
-        }
+    fclose(file);
 
+    bool success = interpret(start_ptr, file_size, r_head, w_head);
+
+    if ( !success ) {
+        printf("Some error occured, fuck off");
+    }
 
     free(start_ptr);
-    fclose(file);
 }
