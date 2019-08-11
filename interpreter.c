c582fcd5fa8a3ee77c1358ccc3f39045a894a5e1
franek <franeklubi@gmail.com>
2019-08-11 19:36:29 +0200

    Add execute function

--- interpreter.c
+++ interpreter.c
@@ -2,12 +2,77 @@
 
 
 bool interpret(
-    char* buffer_ptr, uint32_t buffer_len, Head r_head, Head w_head
+    char* buffer_ptr, uint32_t buffer_len,
+    Head r_head, Head w_head
 ) {
 
+    bool should_end = false;
+    uint32_t index = 0;
 
+    while ( !should_end ) {
 
+        should_end = execute(
+            buffer_ptr, buffer_len, index,
+            r_head, w_head
+        );
 
+        printf("Should end?: %i\n", should_end);
+
+        index++;
+    }
 
     return true;
+}
+
+
+bool execute(
+    char* buffer_ptr, uint32_t buffer_len, uint32_t index,
+    Head r_head, Head w_head
+) {
+    if ( index > buffer_len ) {
+        return true;
+    }
+
+    char opcode = buffer_ptr[index];
+
+    if ( opcode == '\n' ) {
+        return true;
+    }
+
+    switch (opcode) {
+
+        case 'w':
+            printf("Switch to w head\n");
+            break;
+
+        case 'r':
+            printf("Switch to r head\n");
+            break;
+
+        case '\\':
+            printf("Go down\n");
+            break;
+
+        case '/':
+            printf("Go up\n");
+            break;
+
+        case '<':
+            printf("Go right\n");
+            break;
+
+        case '>':
+            printf("Go left\n");
+            break;
+
+        case '.':
+            printf("Print mod\n");
+            break;
+
+        default:
+            printf("Load mod\n");
+            break;
+    }
+
+    return false;
 }
--- interpreter.h
+++ interpreter.h
@@ -3,11 +3,19 @@
 
 #include <stdbool.h>
 #include <stdint.h>
+#include <stdio.h>
 
 #include "Head.h"
 
 
 bool interpret(char* buffer_ptr, uint32_t buffer_len, Head r_head, Head w_head);
+
+
+// execute executes opcode and returns true if the execution should end
+bool execute(
+    char* buffer_ptr, uint32_t buffer_len, uint32_t index,
+    Head r_head, Head w_head
+);
 
 
 #endif
