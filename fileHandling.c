993b5059005be5c9103a6cf9972ca053fd4044fb
franek <franeklubi@gmail.com>
2019-08-15 14:40:46 +0200

    Refactor error messages, variables in insert, and add malloc check in loadFile

--- fileHandling.c
+++ fileHandling.c
@@ -13,11 +13,17 @@ uint32_t getSize(FILE* file) {
 
 
 char* loadFile(FILE* file, uint32_t file_size) {
-    char* start_ptr = (char*) malloc(file_size);
+    void* temp_start_ptr = (char*) malloc(file_size);
+    if ( !temp_start_ptr ) {
+        fprintf(stderr, "Can't malloc in loadFile\n");
+        return temp_start_ptr;
+    }
+    char* start_ptr = (char*) temp_start_ptr;
+
     for ( uint32_t x = 0; x < file_size; x++ ) {
         char read_character = fgetc(file);
         if ( read_character == EOF && x != file_size-1 ) {
-            fprintf(stderr, "Problem with reading file at %i", x);
+            fprintf(stderr, "Problem with reading file at %i\n", x);
         }
         start_ptr[x] = read_character;
     }
@@ -30,32 +36,32 @@ char* insert(char* buffer_ptr, uint32_t* buffer_len, char c, uint32_t index) {
 
     // mitigate this issue by just not giving a fuck
     if ( index >= *buffer_len ) {
-        fprintf(stderr, "Index bigger than buffer len");
+        fprintf(stderr, "Index bigger than buffer len in insert\n");
         return buffer_ptr;
     }
 
     // make another buffer to store the values after index, also - the naming
     // is fine, it's a temporary pointer to a temp buffer
-    void* temp_temp_buffer_ptr = malloc(*buffer_len-index);
-    if ( !temp_temp_buffer_ptr ) {
-        fprintf(stderr, "Can't malloc temp buffer\n");
+    void* temp_partial_buffer_ptr = malloc(*buffer_len-index);
+    if ( !temp_partial_buffer_ptr ) {
+        fprintf(stderr, "Can't malloc temp buffer in insert\n");
         return buffer_ptr;
     }
-    char* temp_buffer_ptr = (char*) temp_temp_buffer_ptr;
+    char* partial_buffer_ptr = (char*) temp_partial_buffer_ptr;
 
     // copy temp values
     for ( uint32_t x = 0; x < *buffer_len - index; x++ ) {
-        temp_buffer_ptr[x] = buffer_ptr[x+index];
+        partial_buffer_ptr[x] = buffer_ptr[x+index];
     }
 
 
     (*buffer_len)++;
     void* temp_new_buffer_ptr = realloc(buffer_ptr, *buffer_len);
     if ( !temp_new_buffer_ptr ) {
-        fprintf(stderr, "Can't realloc\n");
+        fprintf(stderr, "Can't realloc in insert\n");
 
         // don't forget to free the temp buffer silly ;)
-        free(temp_buffer_ptr);
+        free(partial_buffer_ptr);
 
         return buffer_ptr;
     }
@@ -66,13 +72,13 @@ char* insert(char* buffer_ptr, uint32_t* buffer_len, char c, uint32_t index) {
     // replace the index
     new_buffer_ptr[index] = c;
 
-    // unwind the temp_buffer_ptr
+    // unwind the partial_buffer_ptr
     for ( uint32_t x = 0; x < *buffer_len-index; x++ ) {
-        new_buffer_ptr[x+index+1] = temp_buffer_ptr[x];
+        new_buffer_ptr[x+index+1] = partial_buffer_ptr[x];
     }
 
-    // free the temp_buffer_ptr
-    free(temp_buffer_ptr);
+    // free the partial_buffer_ptr
+    free(partial_buffer_ptr);
 
     return new_buffer_ptr;
 }
